a19d67510de9aa23b4ddc3368f8c8ee6f2cb2388
zackees <z@zackees.com>
2022-04-04 22:58:13 -0700

    adds better installation for digital ocean

--- install_digital_ocean.sh
+++ install_digital_ocean.sh
@@ -1,6 +1,6 @@
 apt install -y python-is-python3 python3-pip
 python -m pip install -r requirements.txt
-cp install/app.service /etc/systemd/system/app.service
+cp /root/ytclip-server/install/app.service /etc/systemd/system/app.service
 sudo systemctl daemon-reload
 sudo systemctl enable app.service
 sudo systemctl start app.service
