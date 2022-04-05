c24d4e6741504113b516ba2b4fb074ed45990405
zackees <z@zackees.com>
2022-04-04 19:19:40 -0700

    test

--- app/run.py
+++ app/run.py
@@ -13,16 +13,10 @@ HERE = os.path.abspath(os.path.dirname(__file__))
 # _TIME_BEFORE_RECHECK = 60 * 60
 _TIME_BEFORE_RECHECK = 10
 
-
 def main() -> None:
     """Main entry point."""
     # Start off by updating to the latest version of the repo.
-    stdout = subprocess.check_output(["git", "pull"], cwd=HERE).decode("utf-8")
-    if "Already up to date." in stdout:
-        pass
-    else:
-        print(stdout)
-
+    subprocess.call(["git", "pull"], cwd=HERE)
     # Setup the environment
     env = {"FLASK_APP": "app/app.py", "FLASK_ENV": "development"}
     env.update(os.environ)
