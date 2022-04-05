3a0489b5ed57923d2af89700c0970054784668ef
zackees <z@zackees.com>
2022-04-04 19:08:32 -0700

    nit

--- app/run.py
+++ app/run.py
@@ -6,6 +6,7 @@
 import os
 import subprocess
 import time
+from datetime import datetime
 
 HERE = os.path.dirname(__file__)
 
@@ -27,21 +28,24 @@ def main() -> None:
     # Start off by updating to the latest version of the repo.
     subprocess.call(["git", "pull"], cwd=HERE)
     # Setup the environment
-    env = {"FLASK_APP": "app.py", "FLASK_ENV": "development"}
+    env = {"FLASK_APP": "app/app.py", "FLASK_ENV": "development"}
     env.update(os.environ)
     # Start the flask app
     cmd = ["flask", "run", "--host", "0.0.0.0", "--port", "80"]
-    flask_proc = subprocess.Popen(cmd, env=env, cwd=HERE)
+    flask_proc = subprocess.Popen(cmd, env=env, cwd=os.path.dirname(HERE))
 
     while True:
         time.sleep(_TIME_BEFORE_RECHECK)
         if _check_repo():
+            print(f"Changes in the repo have been detected at {datetime.now()}")
             # The repo has changed so it and the flask app need to be updated.
+            print("Killing the running flask task")
             flask_proc.kill()
             flask_proc.wait()
-            # force a git pull
+            print("Updating the repo")
             cmd = ["git", "pull"]
             subprocess.call(cmd, cwd=HERE)
+            print("Restarting the flask app.")
             flask_proc = subprocess.Popen(cmd, env=env, cwd=HERE)
 
 
