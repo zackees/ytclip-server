9dd0ee9a8627fad61706c6d3940eadbf428f52fe
zackees <z@zackees.com>
2022-04-04 19:23:41 -0700

    nit

--- app/run.py
+++ app/run.py
@@ -10,11 +10,12 @@ import time
 HERE = os.path.abspath(os.path.dirname(__file__))
 
 # Every 60 minutes
-# _TIME_BEFORE_RECHECK = 60 * 60
-_TIME_BEFORE_RECHECK = 10
+_TIME_BEFORE_RECHECK = 60 * 60
+# _TIME_BEFORE_RECHECK = 10
 
 
 def git_pull() -> None:
+    """Silently updates the repo to the latest version."""
     subprocess.call(["git", "pull"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, cwd=HERE)
 
 
@@ -31,6 +32,7 @@ def main() -> None:
         flask_proc = subprocess.Popen(cmd, env=env, cwd=os.path.dirname(HERE))
         while True:
             time.sleep(_TIME_BEFORE_RECHECK)
+            # If changes occure to app.py then flask will immediatly reload.
             git_pull()
     finally:
         flask_proc.terminate()
