d3e4a6b16114a05af33188859f412f1e550fbd8e
zackees <z@zackees.com>
2022-04-04 23:38:43 -0700

    use docker to deploy app

--- install_digital_ocean.sh
+++
@@ -1,8 +0,0 @@
-apt install -y python-is-python3 python3-pip
-python -m pip install -r requirements.txt
-cp /root/ytclip-server/install/app.service /etc/systemd/system/app.service
-sudo systemctl daemon-reload
-sudo systemctl enable app.service
-sudo systemctl start app.service
-sudo systemctl status app.service
-
