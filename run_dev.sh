a687032e0c0442dd4d0651cc912d55294010c6a6
zackees <z@zackees.com>
2022-04-13 11:01:36 -0700

    adds auto browser loading

--- run_dev.sh
+++ run_dev.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/sh
 set -e
 cd $( dirname ${BASH_SOURCE[0]})
+python -m webbrowser -t "http://127.0.0.1:80"
 uvicorn ytclip_server.app:app --no-use-colors --reload --port 80 --host 0.0.0.0
